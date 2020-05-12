89bcac06cced5506b85a17f2a8570e4750228396
robertostoja-lniski <robert.lniski@gmail.com>
2020-05-12 19:07:07 +0200

    Token is a local field

--- include/Parser.h
+++ include/Parser.h
@@ -25,35 +25,39 @@ private:
     //tmp just for one subtree
     std::shared_ptr<Scanner> scanner;
     std::unique_ptr<RepresentationConverter> converter;
-    std::unique_ptr<DeclarationReader> declarationReader;
     // right not used
     std::unique_ptr<FileExpression> mainRoot;
     std::stack <std::shared_ptr<Expression>> recentExpressions;
+    Token token;
+    void parseProgram();
     void addDeclarationsToTree(std::shared_ptr<RootExpression> declaration);
     bool tryToBuildExpression(Token token);
-    bool tryToBuildDeclaration(Token token);
+    std::shared_ptr<RootExpression> tryToBuildDeclaration();
+    bool tryToBuildDeclarationLegacy();
+    std::shared_ptr<TypeSpecifierExpression> getExpressionWithAssignedSpecifier();
+    std::shared_ptr<BodyExpression> getParamsAsManyDeclarations();
     void assignTreeToRoot();
     void transformTokenIntoTreeNode(std::shared_ptr<Token> token);
-    void createIntExpression(Token token);
-    void createFloatExpression(Token token);
-    void createAdditionExpression(Token token);
-    void createMultExpression(Token token);
-    void createAssignExpression(Token token);
-    void createVarNameExpression(Token token);
-    void createFunctionCallExpression(Token token);
-    void createBooleanAndExpression(Token token);
-    void createBooleanOrExpression(Token token);
-    void createBooleanOperatorExpression(Token token);
-    void createSemiconExpression(Token token);
-    void createNoArgFunctionExpression(Token token);
-    void createNextLineExpression(Token token);
-    void createForExpression(Token token);
-    void createIfExpression(Token token);
-    void createElseExpression(Token token);
-    void createWhileExpression(Token token);
-    void createDoExpression(Token token);
-    void createDoneExpression(Token token);
-    void createFieldReferenceExpression(Token token);
+    void createIntExpression();
+    void createFloatExpression();
+    void createAdditionExpression();
+    void createMultExpression();
+    void createAssignExpression();
+    void createVarNameExpression();
+    void createFunctionCallExpression();
+    void createBooleanAndExpression();
+    void createBooleanOrExpression();
+    void createBooleanOperatorExpression();
+    void createSemiconExpression();
+    void createNoArgFunctionExpression();
+    void createNextLineExpression();
+    void createForExpression();
+    void createIfExpression();
+    void createElseExpression();
+    void createWhileExpression();
+    void createDoExpression();
+    void createDoneExpression();
+    void createFieldReferenceExpression();
     void joinUpperStatementsUntilDoFound(std::shared_ptr<BodyExpression> condBody);
     void assignBodyToUpperExpression(std::shared_ptr<BodyExpression> condBody);
     void assignBodyToUpperElse(std::shared_ptr<BodyExpression> condBody);
@@ -63,28 +67,28 @@ private:
 
     Token getTokenValFromScanner();
 
-    std::map<Type, std::function<void(Token)>> tokensToNode {
-            {T_INT_NUM, [&](Token token){createIntExpression(token);}},
-            {T_REAL_NUM, [&](Token token){createFloatExpression(token);}},
-            {T_ADD_OPERATOR, [&](Token token){createAdditionExpression(token);}},
-            {T_MULT_OPERATOR, [&](Token token){createMultExpression(token);}},
-            {T_BOOLEAN_OPERATOR, [&](Token token) {createBooleanOperatorExpression(token);}},
-            {T_BOOLEAN_AND, [&](Token token){createBooleanAndExpression(token);}},
-            {T_SEMICON, [&](Token token){createSemiconExpression(token);}},
-            {T_BOOLEAN_OR, [&](Token token){createBooleanOrExpression(token);}},
-            {T_ASSIGN_OPERATOR, [&](Token token){createAssignExpression(token);}},
-            {T_USER_DEFINED_NAME, [&](Token token){createVarNameExpression(token);}},
-            {T_FUNCTION_NAME, [&](Token token){createFunctionCallExpression(token);}},
-            {T_NO_ARG_FUNCTION_NAME, [&](Token token){createNoArgFunctionExpression(token);}},
-            {T_NEXT_LINE, [&](Token token){createNextLineExpression(token);}},
-            {T_WHILE, [&](Token token){createWhileExpression(token);}},
-            {T_IF, [&](Token token){createIfExpression(token);}},
-            {T_ELSE, [&](Token token){createElseExpression(token);}},
-            {T_FOR, [&](Token token){createForExpression(token);}},
-            {T_DO, [&](Token token){createDoExpression(token);}},
-            {T_DONE, [&](Token token){createDoneExpression(token);}},
-            {T_DOT, [&](Token token){createFieldReferenceExpression(token);}},
-            {T_DUMMY_ARG, [&](Token token){createVarNameExpression(token);}},
+    std::map<Type, std::function<void(void)>> tokensToNode {
+            {T_INT_NUM, [&](){createIntExpression();}},
+            {T_REAL_NUM, [&](){createFloatExpression();}},
+            {T_ADD_OPERATOR, [&](){createAdditionExpression();}},
+            {T_MULT_OPERATOR, [&](){createMultExpression();}},
+            {T_BOOLEAN_OPERATOR, [&]() {createBooleanOperatorExpression();}},
+            {T_BOOLEAN_AND, [&](){createBooleanAndExpression();}},
+            {T_SEMICON, [&](){createSemiconExpression();}},
+            {T_BOOLEAN_OR, [&](){createBooleanOrExpression();}},
+            {T_ASSIGN_OPERATOR, [&](){createAssignExpression();}},
+            {T_USER_DEFINED_NAME, [&](){createVarNameExpression();}},
+            {T_FUNCTION_NAME, [&](){createFunctionCallExpression();}},
+            {T_NO_ARG_FUNCTION_NAME, [&](){createNoArgFunctionExpression();}},
+            {T_NEXT_LINE, [&](){createNextLineExpression();}},
+            {T_WHILE, [&](){createWhileExpression();}},
+            {T_IF, [&](){createIfExpression();}},
+            {T_ELSE, [&](){createElseExpression();}},
+            {T_FOR, [&](){createForExpression();}},
+            {T_DO, [&](){createDoExpression();}},
+            {T_DONE, [&](){createDoneExpression();}},
+            {T_DOT, [&](){createFieldReferenceExpression();}},
+            {T_DUMMY_ARG, [&](){createVarNameExpression();}},
     };
 
 
@@ -93,7 +97,6 @@ public:
     Parser(std::shared_ptr<Scanner> scanner){
         this->scanner = scanner;
         converter = std::make_unique<RepresentationConverter>(scanner);
-        declarationReader = std::make_unique<DeclarationReader>(scanner);
         mainRoot = std::make_unique<FileExpression>();
     };
     void generateTree();
